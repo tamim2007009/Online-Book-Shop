2d381e849512272c37e465cc401122adcad1db50
Tamim <tamim.rauful@gmail.com>
2024-05-21 16:59:04 +0600

    Users default login as role_id 3

--- database/migrations/2024_03_12_000000_create_users_table.php
+++ database/migrations/2024_03_12_000000_create_users_table.php
@@ -24,7 +24,7 @@ class CreateUsersTable extends Migration
             $table->integer('is_active')->default(0);
             $table->rememberToken();
             $table->timestamps();
-            //$table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
             $table->foreign('image_id')->references('id')->on('images')->onDelete('cascade');
         });
     }

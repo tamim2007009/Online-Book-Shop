371576dcd69c262190cad8dd6e938eb93c46f138
Tamim <tamim.rauful@gmail.com>
2024-05-24 08:58:31 +0600

    Seeder file created for initial data insertion

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,8 +18,8 @@ class UsersTableSeeder extends Seeder
         // insert 3 users
         DB::table('users')->insert([
             [
-                'name'=>'Tahmid Nishat',
-                'email'=>'tahmid.ni7@gmail.com',
+                'name'=>'Rauful Islam Tamim',
+                'email'=>'tamim2007009@stud.kuet.ac.bd',
                 'password'=> bcrypt('12345'),
                 'role_id'=> 1,
                 'is_active'=> 1
@@ -38,13 +38,7 @@ class UsersTableSeeder extends Seeder
                 'role_id'=> 3,
                 'is_active'=> 1
             ],
-            [
-                'name'=> 'Mr. Editor',
-                'email'=> 'editor@bookshop.com',
-                'password'=> bcrypt('secret'),
-                'role_id'=> 2,
-                'is_active'=> 1
-            ],
+         
         ]);
     }
 }
